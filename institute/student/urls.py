dc1a0b80a32e9ec14734295c64f4c91f845a2578
Ehsan hosseinnejad <ehsan.hosseinnejad77@gmail.com>
2021-09-20 06:51:54 +0430

    end main dashboear and profile and create admin panel

--- institute/student/urls.py
+++ institute/student/urls.py
@@ -4,5 +4,19 @@ from . import views
 
 app_name = 'student'
 urlpatterns = [   
-    path('', views.IndexView, name='index'),  
+    #path('', views.IndexView, name='index'),  
+    path('panel', views.IndexView, name='index'),
+    path('<str:request_id>/profile', views.Profile, name='profile'),
+    path('login', views.LogIn, name='login'),
+    path('', views.LogIn, name='login'),
+    path('print', views.PrintDegree, name='PrintDegree'),
+    path('salary', views.Salary, name='SalaryPayment'),
+    path('terminfo',views.TermInfo, name='TermInfo'),
+    
+    #Error Page
+    path('400',views.error400,name='400'),
+    path('403',views.error403,name='403'),
+    path('404',views.error404,name='404'),
+    path('500',views.error500,name='500'),
+    path('503',views.error503,name='503'),
 ]
--- institute/student/views.py
+++ institute/student/views.py
@@ -1,6 +1,61 @@
 from django.shortcuts import render
-
+from secretary.models import StudentSiteRegister
+from student.models import StudentRegister 
 # Create your views here.
 
 def IndexView(request):
     return render(request, 'student/index.html')
+
+def Profile(request, request_id):
+    if request.method == 'POST':
+        code = request_id
+        name = request.POST['Name']
+        family = request.POST['FamilyName']
+        address = request.POST['Address']
+        tel = request.POST['Tel']
+        phone = request.POST['Phone']
+        age = request.POST['Age']
+        s = StudentRegister.objects.filter(NationalCode = code ).update(Name = name, FamilyName = family, Address = address, Tele= tel, Phone = phone, Age = age)
+        #s = StudentRegister(NationalCode = code, Name = name, FamilyName = family, Address = address, Tele= tel, Phone = phone, Age = age)
+        #s.save()
+        
+         
+    return render(request, 'student/profile.html')
+
+def LogIn(request):
+    if request.method == 'POST':        
+        username = request.POST['Username']
+        password = request.POST['Password']
+        s = StudentSiteRegister.objects.filter(Username = username ,Password = password).count()
+        if s == 1: 
+            context = {'request_id':str( username)}
+            return render(request, 'student/index.html', context)
+    return render(request, 'student/login.html')
+
+def PrintDegree(request):
+    return render(request, 'student/PrintDegree.html')
+
+def Salary(request):
+    return render(request, 'student/SalaryPayment.html')
+
+def TermInfo(request):
+    return render(request, 'student/TermInfo.html')
+
+
+#Error
+def error400(request):
+    return render(request, 'student/400.html')
+
+def error403(request):
+    return render(request, 'student/403.html')
+
+def error404(request):
+    return render(request, 'student/404.html')
+
+def error500(request):
+    return render(request, 'student/500.html')
+
+def error503(request):
+    return render(request, 'student/503.html')
+
+
