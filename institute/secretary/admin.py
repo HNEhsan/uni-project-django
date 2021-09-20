ecf39c2526d157816cad6ff8d948f9f4a29d4350
Ehsan hosseinnejad <ehsan.hosseinnejad77@gmail.com>
2021-09-20 06:52:26 +0430

    create admin panel

--- institute/secretary/admin.py
+++ institute/secretary/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Term)
+admin.site.register(Lesson)
+admin.site.register(SecretaryRegister)
+admin.site.register(StudentSiteRegister)
+admin.site.register(TeacherSiteRegister)
+admin.site.register(SecretarySiteRegister)
+
+
--- institute/teacher/admin.py
+++ institute/teacher/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+admin.site.register(TeacherRegister)
+admin.site.register(TeacherInfo)
+admin.site.register(Request)
+admin.site.register(SalaryReports)
+admin.site.register(StudentScore)
+
